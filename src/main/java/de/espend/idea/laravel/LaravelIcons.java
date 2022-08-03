ff8ff94cea7be70d3af4ec609748b06b5c274d11
sunaoka <n.sunaoka@amanefactory.co.jp>
2022-08-03 14:58:38 +0900

    Build for 2022.2

--- src/main/java/de/espend/idea/laravel/LaravelIcons.java
+++ src/main/java/de/espend/idea/laravel/LaravelIcons.java
@@ -14,14 +14,14 @@ public class LaravelIcons {
 
     static {
         if(Calendar.getInstance().get(Calendar.MONTH) == Calendar.DECEMBER && Calendar.getInstance().get(Calendar.DAY_OF_MONTH) > 7) {
-            LARAVEL = IconLoader.getIcon("/icons/laravel_dec.png");
+            LARAVEL = IconLoader.getIcon("/icons/laravel_dec.png", LaravelIcons.class);
         } else {
-            LARAVEL = IconLoader.getIcon("/icons/laravel.png");
+            LARAVEL = IconLoader.getIcon("/icons/laravel.png", LaravelIcons.class);
         }
     }
 
-    public static final Icon CONFIG = IconLoader.getIcon("/icons/config_value.png");
-    public static final Icon ROUTE = IconLoader.getIcon("/icons/route.png");
-    public static final Icon TRANSLATION = IconLoader.getIcon("/icons/translation.png");
-    public static final Icon TEMPLATE_CONTROLLER_LINE_MARKER = IconLoader.getIcon("/icons/controller_line_marker.png");
+    public static final Icon CONFIG = IconLoader.getIcon("/icons/config_value.png", LaravelIcons.class);
+    public static final Icon ROUTE = IconLoader.getIcon("/icons/route.png", LaravelIcons.class);
+    public static final Icon TRANSLATION = IconLoader.getIcon("/icons/translation.png", LaravelIcons.class);
+    public static final Icon TEMPLATE_CONTROLLER_LINE_MARKER = IconLoader.getIcon("/icons/controller_line_marker.png", LaravelIcons.class);
 }

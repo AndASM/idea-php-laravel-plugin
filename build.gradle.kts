2073cfbc4ce337f8591a1302032663e89afb3399
Andrew Meadows <andrew.meadows@befuddled.ca>
2022-12-11 13:28:40 -0800

    More 223.* updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("java")
 
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.7.10"
+    id("org.jetbrains.kotlin.jvm") version "1.7.22"
 
     // Gradle IntelliJ Plugin
     id("org.jetbrains.intellij") version "1.10.1"
@@ -23,7 +23,7 @@ repositories {
 // Set the JVM language level used to compile sources and generate files - Java 11 is required since 2020.3
 kotlin {
     jvmToolchain {
-        languageVersion.set(JavaLanguageVersion.of(11))
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 
